5bae9e349db10c2eae789c6028a4150ff73738f1
anmol dhanda <rk352714@gmail.com>
2024-04-08 00:24:28 +0530

    code logic changed and readablity updated

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const all_accordions = document.getElementsByClassName("accordion");
-Array.from(all_accordions).forEach((single_accordion)=>{
-    let accordion_titlebox = single_accordion.querySelector(".accordion-titlebox");
-    accordion_titlebox.addEventListener("click",()=>{
-        Array.from(all_accordions).forEach((each_accordion)=>{
-            // ======================================== when the user clicks on the iterated element's single/each accordion then it will expand otherwise collapsed by default ========================================
-            if(each_accordion !== single_accordion) {
-                // ======================================== if both of the iterations aren't same then hide the existing openeed element's description otherwise show it ========================================
-                each_accordion.classList.remove("showhide-accordion-content");
-            }
-        })
+const all_accordions = document.querySelectorAll(".accordion");
+all_accordions.forEach((single_accordion) => {
+  let accordion_titlebox = single_accordion.querySelector(".accordion-titlebox");
+  accordion_titlebox.addEventListener("click", () => {
+      all_accordions.forEach((each_accordion) => {
+        // ======================================== loop throught all accordions elements again ========================================
+      if (each_accordion !== single_accordion) {
+        // ======================================== check if the current accordion is not the one being clicked then remove the showhide-accordion-content class from the accordion element ========================================
+        each_accordion.classList.remove("showhide-accordion-content");
+      } else {
+        // ======================================== if the current accordion is the one being clicked then toggle the particular showhide-accordion-content class for further operations ========================================
         single_accordion.classList.toggle("showhide-accordion-content");
-    })
-})
+      }
+    });
+  });
+});
